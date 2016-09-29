52d033628f507b6f9bb0c9cda10802d86e9fbebb
黄显宏 <to_top@126.com>
2016-09-29 15:36:25 +0800

    Initial Commit

---
+++ PhotoSync/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoSync
+//
+//  Created by Hong on 9/29/16.
+//  Copyright © 2016 Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoSync/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoSync
+//
+//  Created by Hong on 9/29/16.
+//  Copyright © 2016 Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
